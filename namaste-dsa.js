01191bd5bfed149e05a1a6e5e2f9f81eb26e1451
Ankush <ak9108824@gmail.com>
2026-05-22 13:46:55 +0530

    loops within a loop

--- namaste-dsa.js
+++ namaste-dsa.js
@@ -58,20 +58,28 @@
 // console.log(findSmallest(arr))
 
 // Find the second largest number in an array
-function findSecondLargest(arr){
-  if(arr.length < 2) return null /* edge case */
-  let largest = Number.NEGATIVE_INFINITY
-  let secondLargest = Number.NEGATIVE_INFINITY
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i] > largest){ /* checking if current element is greater than largest */
-     secondLargest = largest 
-     largest = arr[i]
-    } else if(arr[i] < largest && arr[i] > secondLargest){ /* checking if current element is less than largest but greater than second largest */
-      secondLargest = arr[i]
-    }
-  }
-  return secondLargest
-}
+// function findSecondLargest(arr){
+//   if(arr.length < 2) return null /* edge case */
+//   let largest = Number.NEGATIVE_INFINITY
+//   let secondLargest = Number.NEGATIVE_INFINITY
+//   for(let i = 0; i < arr.length; i++){
+//     if(arr[i] > largest){ /* checking if current element is greater than largest */
+//      secondLargest = largest 
+//      largest = arr[i]
+//     } else if(arr[i] < largest && arr[i] > secondLargest){ /* checking if current element is less than largest but greater than second largest */
+//       secondLargest = arr[i]
+//     }
+//   }
+//   return secondLargest
+// }
 
-let arr = [1, 4, 6, 7,11, 9]
-console.log(findSecondLargest(arr))
+// let arr = [1, 4, 6, 7,11, 9]
+// console.log(findSecondLargest(arr))
+
+// Loop within a loop
+for(let i = 0; i < 3; i++){
+  for(let j = 0; j < 3; j++){ /* This loop will run 3 times for each iteration of the outer loop */
+    console.log(`i: ${i}, j: ${j}`) /* this will print all combinations of i and j */
+  }
+}
+// In total this will print 9 combinations of i and j (0,0), (0,1), (0,2), (1,0), (1,1), (1,2), (2,0), (2,1), (2,2)
